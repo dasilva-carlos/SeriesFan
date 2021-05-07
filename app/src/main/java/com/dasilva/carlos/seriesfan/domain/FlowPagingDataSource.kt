4593ee34a9e9c36da239934a4ae2bf7099bfd8ec
Carlos Eduardo <dasilva.carloseduardo@hotmail.com>
2021-05-07 01:10:47 -0300

    Remove CoroutineContext dependency on data source

--- app/src/main/java/com/dasilva/carlos/seriesfan/domain/FlowPagingDataSource.kt
+++ app/src/main/java/com/dasilva/carlos/seriesfan/domain/FlowPagingDataSource.kt
@@ -3,10 +3,8 @@ package com.dasilva.carlos.seriesfan.domain
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.dasilva.carlos.seriesfan.network.data.ResponseException
-import kotlin.coroutines.CoroutineContext
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.single
 
@@ -14,7 +12,6 @@ private const val FIRST_PAGE = 0
 private const val NOT_FOUND_CODE = 404
 
 class FlowPagingDataSource<T : Any, R : Any> (
-    private val coroutineContext: CoroutineContext,
     private val converter: (List<T>) -> List<R>,
     private val flowProvider: (Int) -> Flow<List<T>>
 ) : PagingSource<Int, R>() {
@@ -30,7 +27,7 @@ class FlowPagingDataSource<T : Any, R : Any> (
         val key = params.key ?: FIRST_PAGE
         val prevKey = if (key > FIRST_PAGE) key - 1 else null
 
-        return flowProvider(key).flowOn(coroutineContext)
+        return flowProvider(key)
             .map<List<T>, LoadResult<Int, R>> {
                 return@map LoadResult.Page(
                     data = converter(it),
