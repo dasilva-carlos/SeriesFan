b68943264c2471147ea05caafee1e04c2a8b9fd1
Carlos Eduardo <dasilva.carloseduardo@hotmail.com>
2021-05-07 02:22:04 -0300

    Add view binding

---
+++ app/src/main/java/com/dasilva/carlos/seriesfan/structure/BindingFragment.kt
@@ -0,0 +1,30 @@
+package com.dasilva.carlos.seriesfan.structure
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.fragment.app.Fragment
+
+abstract class BindingFragment<out T>(@LayoutRes layout: Int) : Fragment(layout) {
+
+    abstract val binder: (View) -> T
+
+    private var _binding: T? = null
+    val binding: T?
+        get() = _binding
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = super.onCreateView(inflater, container, savedInstanceState)?.also {
+        _binding = binder(it)
+    }
+}
