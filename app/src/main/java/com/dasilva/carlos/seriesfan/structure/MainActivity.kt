fff77f4b341917d5cf31f9e867ac9a3c20294b3e
Carlos Eduardo <dasilva.carloseduardo@hotmail.com>
2021-05-09 16:42:32 -0300

    Add launch icon and splash screen

--- app/src/main/java/com/dasilva/carlos/seriesfan/structure/MainActivity.kt
+++ app/src/main/java/com/dasilva/carlos/seriesfan/structure/MainActivity.kt
@@ -6,6 +6,7 @@ import com.dasilva.carlos.seriesfan.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_SeriesFan)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
